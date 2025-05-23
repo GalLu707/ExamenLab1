ed99fcca5c8e6b7d7f727a2dbc299859b52a2d33
GalLu707 <zasseuwu@gmail.com>
2025-05-23 14:03:42 -0600

    otro mas

--- Almendarez_Galatea_Exam1/src/almendarez_galatea_exam1/Almendarez_Galatea_Exam1.java
+++ Almendarez_Galatea_Exam1/src/almendarez_galatea_exam1/Almendarez_Galatea_Exam1.java
@@ -15,6 +15,7 @@ import java.util.Random;
 public class Almendarez_Galatea_Exam1 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        Random alea= new Random();//el import del randoom
         obj.useDelimiter("\n");
         int opcionU=0;
         
@@ -36,6 +37,25 @@ public class Almendarez_Galatea_Exam1 {
                     System.out.println("    PIRAMIDE   ");
                     System.out.println("---------------");
                     
+                    System.out.println("Ingrese de cuantas lineas quiere hacer su piramide");
+                    int tamañopira = obj.nextInt();
+                    
+                    int i,k,j;
+                    int numerobase = 1;
+                    
+
+                    for (i = 1; i <= tamañopira; i++) {
+                        int aumento = 0;
+                        for (j = 1; j <= i; j++) {
+                            //pone en una sola linea los numeros
+                            System.out.print(numerobase + " ");
+                            aumento = aumento + numerobase;
+                            //tiene que ir aumentando 2 para que sea impar 
+                            numerobase = numerobase + 2;
+                        }
+                        System.out.println("=" + aumento);
+                    }
+                    
                     break;
                 case 2:
                     System.out.println("---------------");
@@ -49,6 +69,35 @@ public class Almendarez_Galatea_Exam1 {
                     System.out.println(" piedra, papel ");
                     System.out.println("    o tijera   ");
                     System.out.println("---------------");
+                    String ppotpc= "";
+                    boolean jugar = false;
+                    int contador;
+                    
+                    while(!jugar){
+                      System.out.println("Elija una opcion: piedra, papel o tijera");
+                    String opcionppot = obj.next().toLowerCase();
+                    int opcionpc = alea.nextInt(3);
+                    
+                    
+                    if(opcionpc==0){
+                        ppotpc="piedra";   
+                    }else if (opcionpc==1){
+                        ppotpc="papel";
+                    }else{
+                        ppotpc="tijera";
+                    }
+                        System.out.println("el sistema elijio:" + ppotpc);
+                        
+                        if(ppotpc.equals(opcionppot)){
+                            System.out.println("es un empate!!");
+                        }
+                    
+                    
+                    
+                    }
+                    
+                    
+                    
                     
                     break;
                 case 4:
