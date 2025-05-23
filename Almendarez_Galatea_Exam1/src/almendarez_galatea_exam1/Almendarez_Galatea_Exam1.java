b3936de469914dd775457b44a86d3263de7d4fd1
GalLu707 <zasseuwu@gmail.com>
2025-05-23 13:28:21 -0600

    Initial commit

---
+++ Almendarez_Galatea_Exam1/src/almendarez_galatea_exam1/Almendarez_Galatea_Exam1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package almendarez_galatea_exam1;
+
+/**
+ *
+ * @author USER
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+        
+public class Almendarez_Galatea_Exam1 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        obj.useDelimiter("\n");
+        
+        do {
+            System.out.println("----MENU----");
+            System.out.println("1. PIRAMIDE");
+            System.out.println("2. CLAVE");
+            System.out.println("3. JUEGO DE PIEDRA; PAPEL Y TIJERA");
+            System.out.println("4. ADIVINAR");
+            System.out.println("5: SALIR");
+
+            System.out.println("Ingrese la opcion a la que desea entrar:");
+            int opcionU = obj.nextInt();
+            
+        while (opcionU != 5) {
+        
+        }
+        
+    
+        
+        
+  
+   
+    }
+}
